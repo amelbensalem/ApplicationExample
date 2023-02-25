5c45b78e62c390e1069d024bca60d96d5d824496
Amoula <a_bensalem@esi.dz>
2023-02-25 01:05:01 +0100

    feat : project initialization
    this project is a new project to practice Github.
    
    No issue ID.

---
+++ src/applicationexemple/ApplicationExemple.java
@@ -0,0 +1,16 @@
+package applicationexemple;
+
+/**
+ *
+ * @author Amel Bensalem
+ */
+public class ApplicationExemple {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // This project is to practice Github
+    }
+    
+}
